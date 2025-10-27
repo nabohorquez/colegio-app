7848ca866284ae9660fdefbfd149ea27adf530ee
nixonb96 <nixonbf96@gmail.com>
2025-10-27 14:22:22 -0500

    Add migration for permission table and update roles_by_pages to include permission reference

---
+++ database/migrations/2025_10_27_191755_add_column_permission_to_role_page.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('roles_by_pages', function (Blueprint $table) {
+            $table->integer('id_permission')->references('id')->on('permission');
+            $table->foreign('id_permission')->references('id')->on('permission');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('roles_by_pages', function (Blueprint $table) {
+            $table->dropForeign(['id_permission']);
+            $table->dropColumn('id_permission');
+        });
+    }
+};
